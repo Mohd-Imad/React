0565021d9736fb6f3319bb6860585734f9ae3700
Mohd-Imad <mohdimd123@gmail.com>
2022-12-11 12:01:23 +0530

    Organization-Employee-Chart

---
+++ Org-Employee-Chart/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TreeApp from './TreeApp/TreeApp'
+// import Todolist from './Todolist'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+      {/* <Todolist/> */}
+      <TreeApp/>
+    </div>
+  )
+}
+
+export default App
