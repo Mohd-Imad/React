871a2da42406dfa516b4717ebd733cd57d5067f9
Mohd-Imad <mohdimd123@gmail.com>
2022-12-11 23:44:21 +0530

    ok

--- react-form-validations/src/App.js
+++ react-form-validations/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './Navbar/Navbar'
 import RegForm from './Form/RegForm'
+import LoginForm from './Form/LoginForm'
 
 const App = ()=>{
   return <>
-    <RegForm />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/registration' element={<RegForm />} />
+        <Route path='/login' element={<LoginForm />} />
+      </Routes>
+    </Router>
   </>
 }
 
