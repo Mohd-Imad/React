cc46290ce3a0702486de9390acd9424d85e0e1e0
Mohd-Imad <mohdimd123@gmail.com>
2022-12-14 18:32:14 +0530

    okay

--- Org-Employee-Chart/src backup/App.js
+++ Org-Employee-Chart/src backup/App.js
@@ -2,13 +2,11 @@ import React from 'react'
 import TreeApp from './TreeApp/TreeApp'
 // import Todolist from './Todolist'
 import './App.css'
-import Popup from './TreeApp/Popup'
 const App = () => {
   return (
     <div>
       {/* <Todolist/> */}
-      {/* <TreeApp/> */}
-      <Popup />
+      <TreeApp/>
     </div>
   )
 }
