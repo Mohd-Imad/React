d58f2f17f78090774c829d81d58842d0dd38ea39
Mohd-Imad <mohdimd123@gmail.com>
2023-02-01 22:13:30 +0530

    ok

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import Calculator from './Calculator/Calculator'
 import HOCComponent from './HOC Component/HOCComponent'
 import StatevsVariable from './State-vs-Variable/StatevsVariable'
 
 const App = () => {
   return <>
-    <h1>App Comp</h1>
-    <HOCComponent />
-    <StatevsVariable />
+    {/* <HOCComponent />
+    <StatevsVariable /> */}
+    <Calculator />
   </>
 }
 
---
+++ react-practice/src/Calculator/Calculator.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Calculator = () => {
+
+    return <>
+        <h1>Calculator</h1>
+        <div className="calculator-container">
+            <div className="calculator-head">
+                <input type="text" placeholder="0" />
+            </div>
+            <div className="calculator-body">
+                
+            </div>
+        </div>
+    </>
+}
+
+export default Calculator
