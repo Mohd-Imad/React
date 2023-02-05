d3f55216faf74f3860b03bad12b64ce1c89c5ce1
Mohd-Imad <mohdimd123@gmail.com>
2023-02-05 20:57:40 +0530

    ok

--- react-practice/src/Calculator/Calculator.jsx
+++ react-practice/src/Calculator/Calculator.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import './Calculator.css'
 
 const Calculator = () => {
@@ -6,9 +6,14 @@ const Calculator = () => {
     const [input, setInput] = useState('')
     const [val, setVal] = useState(input)
 
+    useEffect(()=>{
+        setVal(...val,input)
+    },[])
+
     let getInp = (inp)=>{
         setInput(inp)
     }
+
 
    
 
@@ -18,7 +23,7 @@ const Calculator = () => {
         <pre>{JSON.stringify(input)}</pre>
         <div className="calculator-container">
             <div className="calculator-head">
-                <input type="text" className="output-box" placeholder="0" value={val} disabled />
+                <input type="text" className="output-box" placeholder="0" value={input} disabled />
             </div>
             <div className="calculator-body">
                 <div className="calculator-row">
@@ -28,25 +33,25 @@ const Calculator = () => {
                     <input type="button" value='/' onClick={getInp.bind(this,'/')} />
                 </div>
                 <div className="calculator-row">
-                    <input type="button" value='7' onClick={getInp.bind(this,7)} />
-                    <input type="button" value='8' onClick={getInp.bind(this,8)} />
-                    <input type="button" value='9' onClick={getInp.bind(this,9)} />
+                    <input type="button" value='7' onClick={getInp.bind(this,'7')} />
+                    <input type="button" value='8' onClick={getInp.bind(this,'8')} />
+                    <input type="button" value='9' onClick={getInp.bind(this,'9')} />
                     <input type="button" value='*' onClick={getInp.bind(this,'*')} />
                 </div>
                 <div className="calculator-row">
-                    <input type="button" value='4' onClick={getInp.bind(this,4)} />
-                    <input type="button" value='5' onClick={getInp.bind(this,5)} />
-                    <input type="button" value='6' onClick={getInp.bind(this,6)} />
+                    <input type="button" value='4' onClick={getInp.bind(this,'4')} />
+                    <input type="button" value='5' onClick={getInp.bind(this,'5')} />
+                    <input type="button" value='6' onClick={getInp.bind(this,'6')} />
                     <input type="button" value='-' onClick={getInp.bind(this,'-')} />
                 </div>
                 <div className="calculator-row">
-                    <input type="button" value='1' onClick={getInp.bind(this,1)} />
-                    <input type="button" value='2' onClick={getInp.bind(this,2)} />
-                    <input type="button" value='3' onClick={getInp.bind(this,3)} />
+                    <input type="button" value='1' onClick={getInp.bind(this,'1')} />
+                    <input type="button" value='2' onClick={getInp.bind(this,'2')} />
+                    <input type="button" value='3' onClick={getInp.bind(this,'3')} />
                     <input type="button" value='+' onClick={getInp.bind(this,'+')} />
                 </div>
                 <div className="calculator-row">
-                    <input type="button" value='0' onClick={getInp.bind(this,0)} />
+                    <input type="button" value='0' onClick={getInp.bind(this,'0')} />
                     <input type="button" value='.' onClick={getInp.bind(this,'.')} />
                     <input type="button" id="equals-to-btn" value='=' />
                 </div>
